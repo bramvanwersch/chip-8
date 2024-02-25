73962e22aa2c9ddc57747cdb47186339bd994539
bram <eckelradekip10@gmail.com>
2024-02-25 12:22:27 +0100

    more instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -33,7 +33,7 @@ impl CPU {
         let op_byte2 = ram.get(p + 1) as u16;
         op_byte1 << 8 | op_byte2
     }
-
+    // convenience functions
     pub fn set_register(&mut self, nr: usize, value: u8) {
         self.registers[nr] = value;
     }
@@ -42,6 +42,7 @@ impl CPU {
         self.registers[nr]
     }
 
+    // run all RAM instructions
     pub fn run(&mut self, ram: &mut RAM) {
         loop {
             let opcode = self.read_opcode(ram);
@@ -57,13 +58,19 @@ impl CPU {
             let d = (opcode & 0x000F) as u8;
 
             let nnn = opcode & 0x0FFF;
-            let _kk = (opcode & 0x00FF) as u8;
+            let kk = (opcode & 0x00FF) as u8;
             // println!("Instruction: {:04X}", opcode);
 
             match (c, x, y, d) {
                 (0, 0, 0, 0) => { return; }
+                (0, 0, 0, 0xE) => self.clear_display(),
                 (0, 0, 0xE, 0xE) => self.ret(),
+                (0x1, _, _, _) => self.jump(nnn),
                 (0x2, _, _, _) => self.call(nnn),
+                (0x3, _, _, _) => self.skip_if_equal_value(x, kk),
+                (0x4, _, _, _) => self.skip_if_not_equal_value(x, kk),
+                (0x5, _, _, 0) => self.skip_if_equal_registers(x, y),
+                (0x6, _, _, _) => self.put_in_register(x, kk),
                 (0x8, _, _, 0x4) => self.add(x, y),
                 (0xA, _, _, _) => self.set_i(nnn),
                 (0xF, _, 0x5, 0x5) => self.copy_to_memory(x, ram),
@@ -72,6 +79,55 @@ impl CPU {
                 // _ => todo!("opcode {:04x}", opcode)
             }
         }
+    }
+
+    fn clear_display(&mut self) {
+        todo!("Needs implementation");
+    }
+
+    fn ret(&mut self) {
+        // return from a system call
+        if self.stack_pointer == 0 {
+            panic!("Stack underflow!");
+        }
+        self.stack_pointer -= 1;
+        self.program_counter = self.stack[self.stack_pointer] as usize;
+    }
+
+    fn jump(&mut self, addr: u16) {
+        self.program_counter = addr as usize;
+    }
+
+    fn call(&mut self, addr: u16) {
+        // call a subroutine
+        if self.stack_pointer >= self.stack.len() {
+            panic!("Stack overflow, max call stack is {}!", STACK_SIZE);
+        }
+        self.stack[self.stack_pointer] = self.program_counter as u16;
+        self.stack_pointer += 1;
+        self.program_counter = addr as usize;
+    }
+
+    fn skip_if_equal_value(&mut self, register: u8, value: u8) {
+        if self.registers[register as usize] == value {
+            self.program_counter += 2;
+        }
+    }
+
+    fn skip_if_not_equal_value(&mut self, register: u8, value: u8){
+        if self.registers[register as usize] != value {
+            self.program_counter += 2;
+        }
+    }
+
+    fn skip_if_equal_registers(&mut self, register1: u8, register2: u8){
+        if self.registers[register1 as usize] == self.registers[register2 as usize]{
+            self.program_counter += 2;
+        }
+    }
+
+    fn put_in_register(&mut self, register: u8, value: u8){
+        self.registers[register as usize] = value;
     }
 
     fn add(&mut self, x: u8, y: u8) {
@@ -87,25 +143,6 @@ impl CPU {
         }
     }
 
-    fn call(&mut self, addr: u16) {
-        // call a subroutine
-        if self.stack_pointer >= self.stack.len() {
-            panic!("Stack overflow, max call stack is {}!", STACK_SIZE);
-        }
-        self.stack[self.stack_pointer] = self.program_counter as u16;
-        self.stack_pointer += 1;
-        self.program_counter = addr as usize;
-    }
-
-    fn ret(&mut self) {
-        // return from a system call
-        if self.stack_pointer == 0 {
-            panic!("Stack underflow!");
-        }
-        self.stack_pointer -= 1;
-        self.program_counter = self.stack[self.stack_pointer] as usize;
-    }
-
     fn set_i(&mut self, nnn: u16) {
         // set special register i to a given value often a memory adres
         self.i = nnn;
@@ -118,7 +155,7 @@ impl CPU {
         }
     }
 
-    fn copy_from_memory(&mut self, x: u8, ram: &mut RAM){
+    fn copy_from_memory(&mut self, x: u8, ram: &mut RAM) {
         for nr in 0..x as usize + 1 {
             self.registers[nr] = ram.get((self.i as usize) + nr);
         }
@@ -140,6 +177,106 @@ mod tests {
     }
 
     #[test]
+    fn test_set_register() {
+        let mut cpu = create_cpu();
+        let mut ram = create_ram();
+        ram.set_u16(0, 0x6009);
+        cpu.run(&mut ram);
+        assert_eq!(cpu.read_register(0), 9);
+    }
+
+    #[test]
+    fn test_jump() {
+        let mut cpu = create_cpu();
+        let mut ram = create_ram();
+        cpu.set_register(0, 1);
+        cpu.set_register(1, 2);
+        ram.set_u16(0, 0x1004);
+        ram.set_u16(2, 0x8014);
+        cpu.run(&mut ram);
+        // make sure that add was not run
+        assert_eq!(cpu.read_register(0), 1);
+    }
+
+    #[test]
+    fn test_skip_if_equal_value1() {
+        // check skip if equal
+        let mut cpu = create_cpu();
+        let mut ram = create_ram();
+        cpu.set_register(0, 1);
+        cpu.set_register(1, 2);
+        ram.set_u16(0, 0x3102);
+        ram.set_u16(2, 0x8014);
+        cpu.run(&mut ram);
+        assert_eq!(cpu.read_register(0), 1);
+    }
+
+    #[test]
+    fn test_skip_if_equal_value2() {
+        // check not skip[ if not equal
+        let mut cpu = create_cpu();
+        let mut ram = create_ram();
+        cpu.set_register(0, 1);
+        cpu.set_register(1, 2);
+        ram.set_u16(0, 0x3103);
+        ram.set_u16(2, 0x8014);
+        cpu.run(&mut ram);
+        assert_eq!(cpu.read_register(0), 3);
+    }
+
+    #[test]
+    fn test_skip_if_not_equal_value1() {
+        // check skip if equal
+        let mut cpu = create_cpu();
+        let mut ram = create_ram();
+        cpu.set_register(0, 1);
+        cpu.set_register(1, 6);
+        ram.set_u16(0, 0x4102);
+        ram.set_u16(2, 0x8014);
+        cpu.run(&mut ram);
+        assert_eq!(cpu.read_register(0), 1);
+    }
+
+    #[test]
+    fn test_skip_if_not_equal_value2() {
+        // check not skip[ if not equal
+        let mut cpu = create_cpu();
+        let mut ram = create_ram();
+        cpu.set_register(0, 2);
+        cpu.set_register(1, 3);
+        ram.set_u16(0, 0x4002);
+        ram.set_u16(2, 0x8014);
+        cpu.run(&mut ram);
+        assert_eq!(cpu.read_register(0), 5);
+    }
+
+    #[test]
+    fn test_skip_if_equal_registers1() {
+        // check skip if equal
+        let mut cpu = create_cpu();
+        let mut ram = create_ram();
+        cpu.set_register(0, 1);
+        cpu.set_register(1, 1);
+        ram.set_u16(0, 0x5010);
+        ram.set_u16(2, 0x8014);
+        cpu.run(&mut ram);
+        assert_eq!(cpu.read_register(0), 1);
+    }
+
+    #[test]
+    fn test_skip_if_equal_registers2() {
+        // check not skip[ if not equal
+        let mut cpu = create_cpu();
+        let mut ram = create_ram();
+        cpu.set_register(0, 1);
+        cpu.set_register(1, 3);
+        ram.set_u16(0, 0x5010);
+        ram.set_u16(2, 0x8014);
+        cpu.run(&mut ram);
+        assert_eq!(cpu.read_register(0), 4);
+    }
+
+        #[test]
     fn test_call() {
         // test both call and ret
         let mut cpu = create_cpu();
